1e9c2ead723d2d3fdf1acf5e760669b1cb19f26e
Raehyeon Jeong <rjeong@student.42seoul.kr>
2023-05-29 06:13:53 +0900

    Feat : 홀수 번째 철학자는 왼쪽 포크 먼저, 짝수 번째 철학자는 오른쪽 포크 먼저 집는 방법 추가

--- philo/philo.c
+++ philo/philo.c
@@ -113,14 +113,8 @@ void	print_state(t_shared *shared, t_philo *philo, char *state)
 	pthread_mutex_unlock(&shared->sim.lock);
 }
 
-void	eat(t_shared *shared, t_philo *philo)
+void	act_eat(t_shared *shared, t_philo *philo)
 {
-	pthread_mutex_lock(&(shared->forks + philo->left_fork_id)->lock);
-	(shared->forks + philo->left_fork_id)->fork_status = USING;
-	print_state(shared, philo, STATE_TAKE);
-	pthread_mutex_lock(&(shared->forks + philo->right_fork_id)->lock);
-	(shared->forks + philo->right_fork_id)->fork_status = USING;
-	print_state(shared, philo, STATE_TAKE);
 	pthread_mutex_lock(&philo->death_time.lock);
 	philo->death_time.death_time = get_time() + philo->info->time_to_die;
 	pthread_mutex_unlock(&philo->death_time.lock);
@@ -131,13 +125,48 @@ void	eat(t_shared *shared, t_philo *philo)
 	{
 		pthread_mutex_lock(&shared->full_philo_cnt.lock);
 		++shared->full_philo_cnt.full_philo_cnt;
+		if (shared->full_philo_cnt.full_philo_cnt
+			== philo->info->number_of_philosophers)
+		{
+			pthread_mutex_lock(&(shared->sim.lock));
+			shared->sim.sim_status = OFF;
+			pthread_mutex_unlock(&(shared->sim.lock));
+		}
 		pthread_mutex_unlock(&shared->full_philo_cnt.lock);
 	}
 	usleep(philo->info->time_to_eat * 1000);
+}
+
+
+void	act_take_fork_left(t_shared *shared, t_philo *philo)
+{
+	pthread_mutex_lock(&(shared->forks + philo->left_fork_id)->lock);
+	(shared->forks + philo->left_fork_id)->fork_status = USING;
+	print_state(shared, philo, STATE_TAKE);
+	pthread_mutex_lock(&(shared->forks + philo->right_fork_id)->lock);
+	(shared->forks + philo->right_fork_id)->fork_status = USING;
+	print_state(shared, philo, STATE_TAKE);
+	act_eat(shared, philo);
 	(shared->forks + philo->left_fork_id)->fork_status = FREE;
 	pthread_mutex_unlock(&(shared->forks + philo->left_fork_id)->lock);
 	(shared->forks + philo->right_fork_id)->fork_status = FREE;
 	pthread_mutex_unlock(&(shared->forks + philo->right_fork_id)->lock);
+}
+
+
+void	act_take_fork_right(t_shared *shared, t_philo *philo)
+{
+	pthread_mutex_lock(&(shared->forks + philo->right_fork_id)->lock);
+	(shared->forks + philo->right_fork_id)->fork_status = USING;
+	print_state(shared, philo, STATE_TAKE);
+	pthread_mutex_lock(&(shared->forks + philo->left_fork_id)->lock);
+	(shared->forks + philo->left_fork_id)->fork_status = USING;
+	print_state(shared, philo, STATE_TAKE);
+	act_eat(shared, philo);
+	(shared->forks + philo->right_fork_id)->fork_status = FREE;
+	pthread_mutex_unlock(&(shared->forks + philo->right_fork_id)->lock);
+	(shared->forks + philo->left_fork_id)->fork_status = FREE;
+	pthread_mutex_unlock(&(shared->forks + philo->left_fork_id)->lock);
 }
 
 void	*philo_act(void *arg)
@@ -147,12 +176,20 @@ void	*philo_act(void *arg)
 	philo = (t_philo *)arg;
 	while (get_time() < philo->info->start_time)
 		;
-	while (1)
+	if (philo->id % 2)
 	{
-		eat(&(philo->info->shared), philo);
+		while (1)
+		{
+			act_take_fork_left(&(philo->info->shared), philo);
+		}
+	}
+	else
+	{
 
-		usleep(100);
-
+		while (1)
+		{
+			act_take_fork_right(&(philo->info->shared), philo);
+		}
 	}
 	return (0);
 }
