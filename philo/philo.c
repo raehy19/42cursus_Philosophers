a89307f7998ed0b31868ed769d51237a20b05aed
Raehyeon Jeong <raehy19@gmail.com>
2023-05-28 23:26:59 +0900

    Feat : add checking parsed argument method

--- philo/philo.c
+++ philo/philo.c
@@ -20,6 +20,17 @@ int	ft_arg_err(void)
 	return(-1);
 }
 
+int	ft_check_arg(int argc, t_info *info)
+{
+	if (info->number_of_philosophers < 1
+		|| info->time_to_die < 0
+		|| info->time_to_eat < 0
+		|| info->time_to_sleep < 0
+		|| ((argc == 6) && (info->number_of_times_each_philosopher_must_eat < 0)))
+		return (-1);
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	static t_info	info;
@@ -30,8 +41,12 @@ int	main(int argc, char **argv)
 	info.time_to_die = ft_atoi(*(argv + 2));
 	info.time_to_eat = ft_atoi(*(argv + 3));
 	info.time_to_sleep = ft_atoi(*(argv + 4));
+	info.number_of_times_each_philosopher_must_eat = -1;
 	if (argc == 6)
 		info.number_of_times_each_philosopher_must_eat = ft_atoi(*(argv + 5));
+	if (ft_check_arg(argc, &info))
+		return (ft_arg_err());
+
 	printf("%d %d %d %d %d\n",info.number_of_philosophers, info.time_to_die, info.time_to_eat, info.time_to_sleep, info.number_of_times_each_philosopher_must_eat);
 	return (0);
 }
